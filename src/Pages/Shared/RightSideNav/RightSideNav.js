564aa816f441685485ab6e303901ce32972efe3b
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-03-27 16:55:41 +0600

    sdfgasdfgsdfa

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2> This is Right Side</h2>
+        <ButtonGroup vertical>
+            <Button  variant="outline-primary mb-2"><FaGoogle/> Sign in with Google</Button>
+            <Button  variant="outline-dark"><FaGithub/> Sign in with GitHub</Button>
+      </ButtonGroup>
         </div>
     );
 };
