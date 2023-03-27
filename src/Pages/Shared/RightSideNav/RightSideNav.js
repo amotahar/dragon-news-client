ff50a2279034cb01ff20b7570c4325ff4e9b76bf
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-03-27 17:16:37 +0600

    sgfdsgdsfghsdf

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import { FaGoogle } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import { FaGoogle, FaGithub, FaFacebook, FaWhatsapp, FaTwitter, FaTwitch } from "react-icons/fa";
+import ListGroup from 'react-bootstrap/ListGroup';
+import BrandCarousel from '../BrandCarousel/BrandCarousel';
+
 
 
 const RightSideNav = () => {
     return (
         <div>
-        <ButtonGroup vertical>
-            <Button  variant="outline-primary mb-2"><FaGoogle/> Sign in with Google</Button>
-            <Button  variant="outline-dark"><FaGithub/> Sign in with GitHub</Button>
-      </ButtonGroup>
+                <ButtonGroup vertical>
+                        <Button className='mb-2' variant="outline-primary"><FaGoogle/> Login with Google </Button>
+                        <Button  variant="outline-dark"><FaGithub/> Login with GitHub</Button>
+                </ButtonGroup>
+                <div>
+                    <h4>Find us on</h4>
+                    <ListGroup>
+                        <ListGroup.Item className='mb-2'><FaFacebook/>  Facebook</ListGroup.Item>
+                        <ListGroup.Item className='mb-2'><FaWhatsapp/>  Whatsapp</ListGroup.Item>
+                        <ListGroup.Item className='mb-2'><FaTwitter/>  Twitter</ListGroup.Item>
+                        <ListGroup.Item className='mb-2'><FaTwitch/>  Twitch</ListGroup.Item>
+                        <ListGroup.Item className='mb-2'>   Vestibulum at eros</ListGroup.Item>
+                    </ListGroup>
+                </div>
+
+                <div>
+                      <BrandCarousel></BrandCarousel>
+                </div>
+
+                
         </div>
     );
 };
