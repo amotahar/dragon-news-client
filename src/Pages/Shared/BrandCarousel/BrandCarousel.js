7db1fac184ba5007a7d76ee41ddf49254dc5f3da
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-03-27 18:47:37 +0600

    fgsdfgsfd

--- src/Pages/Shared/BrandCarousel/BrandCarousel.js
+++ src/Pages/Shared/BrandCarousel/BrandCarousel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-
+import Brand1 from '../../../assets/brand/Brand1.png';
+import Brand2 from '../../../assets/brand/Brand2.png';
 
 const BrandCarousel = () => {
     return (
@@ -9,39 +10,15 @@ const BrandCarousel = () => {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+            src={Brand1}
+            alt="First slide"/>         
         </Carousel.Item>
+        
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
-  
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
-  
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
+            src={Brand2}
+            alt="Second slide"/>
         </Carousel.Item>
       </Carousel>
         </div>
