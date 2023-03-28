ae963958c6a5936eb243a77f30743abb0ba52f54
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-03-28 15:17:59 +0600

    afsdagdsag

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { Outlet } from 'react-router-dom';
-import Footer from '../Pages/Shared/Footer/Footer';
-import Header from '../Pages/Shared/Header/Header';
-import LeftSideNav from '../Pages/Shared/LeftSideNav/LeftSideNav';
-import RightSideNav from '../Pages/Shared/RightSideNav/RightSideNav';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import Header from "../Pages/Shared/Header/Header";
+import LeftSideNav from "../Pages/Shared/LeftSideNav/LeftSideNav";
+import RightSideNav from "../Pages/Shared/RightSideNav/RightSideNav";
 
 const Main = () => {
-    return (
-        <div>
-        <Header></Header>
-        <Container>
+  return (
+    <div>
+      <Header></Header>
+      <Container>
         <Row>
-           <Col lg="2" className='d-none d-lg-block'>
-              <LeftSideNav></LeftSideNav>
-           </Col>
-           <Col lg="7">
-              <Outlet></Outlet>
-           </Col>
-           <Col lg="3">
-           <RightSideNav></RightSideNav>
-           </Col>
+          <Col lg="2" className="d-none d-lg-block">
+            <LeftSideNav></LeftSideNav>
+          </Col>
+          <Col lg="7">
+            <Outlet></Outlet>
+          </Col>
+          <Col lg="3">
+            <RightSideNav></RightSideNav>
+          </Col>
         </Row>
-     </Container>
-     <Footer></Footer>
-        </div>
-    );
+      </Container>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
---
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsSummaryCard = ({news}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NewsSummaryCard;
