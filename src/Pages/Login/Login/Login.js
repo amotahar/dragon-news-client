77644e9eec023294f8497729a2f2939f9cc9f65c
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-03-31 02:26:16 +0600

    dfdsfghdfsgh

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,15 +1,34 @@
 import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Login = () => {
     const {signIn} = useContext(AuthContext);
+    const navigate = useNavigate();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email= form.email.value;
+        const password = form.password.value;
+        // console.log(name, password);
+        signIn(email, password)
+        .then((result) => {
+            const user = result.user;
+            console.log(user);
+            form.reset();
+            navigate('/')
+
+        })
+        .catch((err) => console.error(err))
+
+    }
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control name="name" type="email" placeholder="Enter email" required />
+          <Form.Control name="email" type="email" placeholder="Enter email" required />
         </Form.Group>
   
         <Form.Group className="mb-3" controlId="formBasicPassword">
