81972a6bdbd3a64008de55616e352d1002559474
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-03-30 13:33:58 +0600

    fdgsagfsa

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,9 +1,22 @@
 import React, { createContext } from 'react';
+import { getAuth, signInWithPopup } from "firebase/auth";
+import app from '../../firebase/firebase.config';
 
 export const AuthContext = createContext();
+const auth = getAuth(app)
+
 const AuthProvider = ({children}) => {
+
     const user = {displayName: 'Bats Ali'}
-    const authInfo = user;
+
+    const providerLogin = (provider) => {
+      return signInWithPopup(auth, provider)
+
+    }
+
+    const authInfo = { user, providerLogin};
+
+
 
     return (
         <AuthContext.Provider value={authInfo}>
