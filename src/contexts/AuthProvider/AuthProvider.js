7e73194fa0eeb9be55bf2f19ba9d4161d66114da
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-03-30 13:55:11 +0600

    dfgsdfghdsfgsdfg

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,18 +1,27 @@
-import React, { createContext } from 'react';
-import { getAuth, signInWithPopup } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged, signInWithPopup } from "firebase/auth";
 import app from '../../firebase/firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
-
-    const user = {displayName: 'Bats Ali'}
-
+    
+    const [ user, setUser] = useState(null);
+     
     const providerLogin = (provider) => {
       return signInWithPopup(auth, provider)
-
     }
+
+    useEffect(()=>{
+       const unsubscribe = onAuthStateChanged(auth,(currentUser) => {
+        console.log("user on side change",currentUser);
+        setUser(currentUser);
+        }); 
+        return () => {
+            unsubscribe();
+        }
+        },[])
 
     const authInfo = { user, providerLogin};
 
