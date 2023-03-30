18da3c4e19a74e2791c567b056d6f825b1d249a6
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-03-30 12:52:16 +0600

    dfgdfsghdfshgdsfgh

---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,15 @@
+import React, { createContext } from 'react';
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+    const user = {displayName: 'Batasa Ali'}
+    const authInfo = {user};
+    return (
+            <AuthContext.Provider  value={authInfo}>
+               {children}
+            </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
